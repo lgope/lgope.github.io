edeb36f285c66cdd4a4da46c3cf98db1f19b043b
lgope <ilakshmangope@gmail.com>
2023-07-31 21:50:35 +0600

    Project link and padding style issue fixed

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -5,31 +5,25 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles/project.css";
 
-const Project = (props) => {
-	const { logo, title, description, linkText, link } = props;
-
-	return (
-		<React.Fragment>
-			<div className="project">
-				<Link to={link}>
-					<div className="project-container">
-						<div className="project-logo">
-							<img src={logo} alt="logo" />
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
-
-							<div className="project-link-text">{linkText}</div>
-						</div>
+const Project = ({ logo, title, description, linkText, link }) => (
+	<div className="project">
+		<Link to={link} target="_blank">
+			<div className="project-container">
+				<div className="project-logo">
+					<img src={logo} alt="logo" />
+				</div>
+				<div className="project-title">{title}</div>
+				<div className="project-description">{description}</div>
+				<div className="project-link">
+					<div className="project-link-icon">
+						<FontAwesomeIcon icon={faLink} />
 					</div>
-				</Link>
+
+					<div className="project-link-text">{linkText}</div>
+				</div>
 			</div>
-		</React.Fragment>
-	);
-};
+		</Link>
+	</div>
+);
 
 export default Project;
